e429a88187b8f2b86755c502c5fde5f69f441656
Fred <guru.three@gmail.com>
2021-06-04 22:25:14 +0100

    changing type of sensor time depending on if using cycles or micros

--- lighthouse1/Sensor.h
+++ lighthouse1/Sensor.h
@@ -31,6 +31,13 @@
   #define digitalWriteFastLOW(pin) {digitalWriteFast(pin, LOW);}
 #endif
 
+// getCycleCount is uint32_t if that's being used for timing, while unsigned long is 64 bit on ESP
+#if defined(ESP32) || defined(ESP8266)
+  #define sensor_time_t uint32_t
+#else
+  #define sensor_time_t unsigned long
+#endif
+
 class SensorBase
 {
 public:
@@ -48,8 +55,8 @@ template<uint8_t SENSOR_PIN> class Sensor: public SensorBase
 
     // pulse start times and lengths (in micros())
     struct Pulse { // BEEP
-      unsigned long pulse_start;
-      unsigned long pulse_length; // should maybe change this to pulse_end to avoid some math in the interrupt?
+      sensor_time_t pulse_start;
+      sensor_time_t pulse_length; // should maybe change this to pulse_end to avoid some math in the interrupt?
       Pulse() : pulse_start(0), pulse_length(0) {} // this should default to values of 0 when created
     };
 
@@ -58,12 +65,12 @@ template<uint8_t SENSOR_PIN> class Sensor: public SensorBase
     
     // state of pulse details (controlled by interrupt)
     struct SensorState {
-      unsigned long sync_pulse_start = 0; // time of most recent pulse
+      sensor_time_t sync_pulse_start = 0; // time of most recent pulse
       AxisType active_axis = SWEEP; // the direction the most recent pulse went
 
       // working variables pre-declared for faster operation
-      unsigned long lasttime = 0; // the last time a pulse was receieved
-      unsigned long rightnow = 0; // time of the interrupt
+      sensor_time_t lasttime = 0; // the last time a pulse was receieved
+      sensor_time_t rightnow = 0; // time of the interrupt
     
       Pulse measured_pulses[BUFFER_LENGTH];
       uint8_t read_index = 0; // next Pulse to be written to
@@ -75,8 +82,8 @@ template<uint8_t SENSOR_PIN> class Sensor: public SensorBase
     static volatile SensorState current_state;
 
     static const uint8_t NUM_TIMINGS = 8;
-    static const unsigned long sync_timings_low[NUM_TIMINGS];
-    static const unsigned long sync_timings_high[NUM_TIMINGS];
+    static const sensor_time_t sync_timings_low[NUM_TIMINGS];
+    static const sensor_time_t sync_timings_high[NUM_TIMINGS];
 
     static volatile int8_t led; // flash something when receiving
 
@@ -92,9 +99,9 @@ template<uint8_t SENSOR_PIN> class Sensor: public SensorBase
 
 
       #ifdef ESP8266
-        current_state.rightnow = ((unsigned long)(ESP.getCycleCount())) * 6.25; // 6.25 ticks per ns (may be faster to just use micros?)
+        current_state.rightnow = (ESP.getCycleCount()) * 6.25; // 6.25 ticks per ns (may be faster to just use micros?)
       #elif ESP32
-        current_state.rightnow = ((unsigned long)(ESP.getCycleCount())) << 2; // 4 ticks per ns
+        current_state.rightnow = (ESP.getCycleCount()) << 2; // 4 ticks per ns
       #else
         current_state.rightnow = micros()*1000; // ns timings
       #endif
@@ -103,7 +110,7 @@ template<uint8_t SENSOR_PIN> class Sensor: public SensorBase
         current_state.measured_pulses[current_state.write_index].pulse_start = current_state.lasttime;
         current_state.measured_pulses[current_state.write_index].pulse_length = current_state.rightnow - current_state.lasttime;
         current_state.write_index++;
-        if (current_state.write_index >= BUFFER_LENGTH)
+        if (current_state.write_index >= BUFFER_LENGTH) // intentionally use uint8_t wrapping?
           current_state.write_index = 0;
       }
       current_state.lasttime = current_state.rightnow;
@@ -175,7 +182,7 @@ public:
         if (current_state.measured_pulses[current_state.read_index].pulse_length < 20000) { // a bit bigger because depending on funny angles the beam may be a bit wider? hopefully better closer and at extreme angles
           identifiedPulse = true;
   
-          unsigned long relative_time = (current_state.measured_pulses[current_state.read_index].pulse_start - current_state.sync_pulse_start);
+          sensor_time_t relative_time = (current_state.measured_pulses[current_state.read_index].pulse_start - current_state.sync_pulse_start);
           if (relative_time < 1222222 || relative_time > 6777777) {
             // do nothing for now, this is likely an impossible reading
           }
@@ -220,9 +227,9 @@ public:
 
 #define SYNC_WINDOW 4500
 template<uint8_t SENSOR_PIN>
-const unsigned long Sensor<SENSOR_PIN>::sync_timings_low[Sensor<SENSOR_PIN>::NUM_TIMINGS] = {62500-SYNC_WINDOW, 72900-SYNC_WINDOW, 83300-SYNC_WINDOW, 93800-SYNC_WINDOW, 104000-SYNC_WINDOW, 115000-SYNC_WINDOW, 125000-SYNC_WINDOW, 135000-SYNC_WINDOW};
+const sensor_time_t Sensor<SENSOR_PIN>::sync_timings_low[Sensor<SENSOR_PIN>::NUM_TIMINGS] = {62500-SYNC_WINDOW, 72900-SYNC_WINDOW, 83300-SYNC_WINDOW, 93800-SYNC_WINDOW, 104000-SYNC_WINDOW, 115000-SYNC_WINDOW, 125000-SYNC_WINDOW, 135000-SYNC_WINDOW};
 template<uint8_t SENSOR_PIN>
-const unsigned long Sensor<SENSOR_PIN>::sync_timings_high[Sensor<SENSOR_PIN>::NUM_TIMINGS] = {62500+SYNC_WINDOW, 72900+SYNC_WINDOW, 83300+SYNC_WINDOW, 93800+SYNC_WINDOW, 104000+SYNC_WINDOW, 115000+SYNC_WINDOW, 125000+SYNC_WINDOW, 135000+SYNC_WINDOW};
+const sensor_time_t Sensor<SENSOR_PIN>::sync_timings_high[Sensor<SENSOR_PIN>::NUM_TIMINGS] = {62500+SYNC_WINDOW, 72900+SYNC_WINDOW, 83300+SYNC_WINDOW, 93800+SYNC_WINDOW, 104000+SYNC_WINDOW, 115000+SYNC_WINDOW, 125000+SYNC_WINDOW, 135000+SYNC_WINDOW};
 
 // volatile as the interrupt will be accessing in ways potentially unforseen by the compiler, so certain optimisations should be avoided
 // https://arduino.stackexchange.com/questions/76000/use-isrs-inside-a-library-more-elegantly
